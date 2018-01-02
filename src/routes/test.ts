7a34243205a07afab856a2ce5c4cd39e51f2f09d
Jacob Parra <jparra@comparaonline.com>
2018-01-02 18:36:06 -0300

    Cleans repo

--- src/routes/test.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-
-const router = express.Router();
-
-router.get('/:name', async (req, res) => {
-  res.send(`Hello ${req.params.name}`);
-});
-
-export default router;
